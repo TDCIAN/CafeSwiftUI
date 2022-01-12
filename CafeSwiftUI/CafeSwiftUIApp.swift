dc964fbd798ba5716cc34c40666efcac8525b564
TDCIAN <tdcian71@gmail.com>
2022-01-13 00:17:55 +0900

    add tabview

--- CafeSwiftUI/CafeSwiftUIApp.swift
+++ CafeSwiftUI/CafeSwiftUIApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct CafeSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
+                .accentColor(.green)
         }
     }
 }
