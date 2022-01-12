91c57adebd64e2f1003210b00498a84d5e668188
TDCIAN <tdcian71@gmail.com>
2022-01-12 23:59:55 +0900

    first commit

---
+++ CafeSwiftUI/CafeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeSwiftUIApp.swift
+//  CafeSwiftUI
+//
+//  Created by JeongminKim on 2022/01/12.
+//
+
+import SwiftUI
+
+@main
+struct CafeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
